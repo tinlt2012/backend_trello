79f95afd80694a2bbbce37eabf5c9e7a6b30d6c9
NALS_MACBOOK_123 <nals_macbook_123@NALS-MACBOOK-123s-MacBook-Pro.local>
2024-08-05 17:05:42 +0700

    connect DB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,22 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+//lttin2012/JP8zStRPYEVzd4Wh
+const MONGODB_URI = 'mongodb+srv://lttin2012:JP8zStRPYEVzd4Wh@cluster0-tinlt.5z9bt4s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-Tinlt'
+const DATABASE_Name ='Trello'
+import {MongoClient, ServerApiVersion} from 'mongodb'
+let trelloDatabaseInstance = null
+// Khoi tao doi tuong Client de connect MongoDb
+const mongoClientInstance = new MongoClient (MONGODB_URI , {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+// Ket noi den Database
+export const CONNECT_DB = async () => {
+    await mongoClientInstance.connect()
+    trelloDatabaseInstance = mongoClientInstance.db(DATABASE_Name)
+}
+export const GET_DB = () =>{
+    if(!trelloDatabaseInstance) throw new Error('Fail')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,36 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
-
-const app = express()
-
-const hostname = 'localhost'
-const port = 8017
-
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
+import {CONNECT_DB,GET_DB} from '~/config/mongodb'
+const START_SERVER = () =>{
+  const app = express()
+  const hostname = 'localhost'
+  const port = 8017
+  app.get('/', (req, res) => {
   res.end('<h1>Hello World!</h1><hr>')
-})
+  })
+  
+  app.listen(port, hostname, async () => {
+    // eslint-disable-next-line no-console
+    console.log(await GET_DB().listCollections().toArray())
+    console.log(` I am running at ${ hostname }:${ port }/`)
+  })
+}
+// CONNECT_DB()
+// .then(() => { console.log('Ket noi thanh cong')})
+// .then(() =>START_SERVER() )
+// .catch(err => {
+//   console.log('error', err)
+// })
+(async () =>{
+  try {
+    console.log('Connecting to Atlas...')
+    await CONNECT_DB()
+    console.log('Ket noi thanh cong')
+     START_SERVER()
+    
+  } catch (error) {
+    console.log('error', err)
+  }
+})(
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+)
